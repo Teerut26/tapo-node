c86dccbe5fac542cfd44a13d394499da2529e90e
teerut26 <teerutsr@gmail.com>
2021-08-13 21:40:15 +0700

    1.0.0

---
+++ README.md
@@ -0,0 +1,141 @@
+
+# API Reference Update : 13/8/21
+
+## Import 
+
+```javascript
+const { TapoLogin, Tapo } = require("./modules.js");
+```
+## Create Instance 
+
+#### Get Token With Email & Password
+```javascript
+let tapoLogin = new TapoLogin("email","password")
+
+tapoLogin.getToken().then((result) => {
+  console.log(result);
+}).catch((err) => {
+  console.log(err);
+});
+```
+
+```javascript
+let tapo = new Tapo("token");
+```
+## Use Methods
+
+#### getCameraLists()
+```javascript
+//example
+tapo.getCameraLists().then((result) => {
+  console.log(result);
+}).catch((err) => {
+  console.log(err);
+});
+
+
+//response
+{
+  totalNum: 1,
+  deviceList: [
+    {
+      deviceType: 'SMART.IPCAMERA',
+      role: 0,
+      fwVer: '1.0.10 Build 200520 Rel.45325n(4555)',
+      appServerUrl: 'https://n-aps1-wap-gw.tplinkcloud.com',
+      deviceRegion: 'ap-southeast-1',
+      deviceId: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+      deviceName: 'C200',
+      deviceHwVer: '1.0',
+      alias: 'HOME',
+      deviceMac: 'XXXXXXXXXXXX',
+      oemId: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+      deviceModel: 'C200',
+      hwId: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+      fwId: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+      isSameRegion: true,
+      status: 1
+    }
+  ],
+  currentIndex: 1
+}
+```
+
+#### getCameraDetail(deviceId)
+```javascript
+//example
+tapo.getCameraDetail("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX").then((result) => {
+  console.log(result);
+}).catch((err) => {
+  console.log(err);
+});
+
+
+//response
+{
+  result: { link_type: 'wifi', rssi: '3', rssiValue: -57, ssid: 'XXXXXXX' },
+  method: 'getConnectionType',
+  error_code: 0
+}
+```
+
+#### getHanddiskInfo(deviceId)
+```javascript
+//example
+tapo.getHanddiskInfo("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX").then((result) => {
+  console.log(result);
+}).catch((err) => {
+  console.log(err);
+});
+
+
+//response
+{
+  video_total_space: '118.3GB',
+  disk_name: '1',
+  loop_record_status: '1',
+  msg_push_total_space: '210.0MB',
+  msg_push_free_space: '210.0MB',
+  type: 'local',
+  picture_total_space: '1.0MB',
+  percent: '100',
+  detect_status: 'normal',
+  write_protect: '0',
+  rw_attr: 'rw',
+  record_start_time: '1600835303',
+  video_free_space: '0B',
+  record_free_duration: '0',
+  total_space: '119.1GB',
+  picture_free_space: '1.0MB',
+  record_duration: '1318832',
+  free_space: '467.0MB',
+  status: 'normal'
+}
+```
+
+#### getPlayback(deviceId,getPlayback,end_date)
+```javascript
+//example
+tapo.getPlayback("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX","20210801","20210813").then((result) => {
+  console.log(result);
+}).catch((err) => {
+  console.log(err);
+});
+
+
+//response
+{
+  resultsCount: 13,
+  results: [
+    '20210801', '20210802',
+    '20210803', '20210804',
+    '20210805', '20210806',
+    '20210807', '20210808',
+    '20210809', '20210810',
+    '20210811', '20210812',
+    '20210813'
+  ]
+}
+```
+
+
---
+++ modules.js
@@ -0,0 +1,219 @@
+const axios = require("axios");
+const https = require("https");
+
+class Config {
+  constructor() {
+    this.url =
+      "https://n-wap-gw.tplinkcloud.com/?appName=TP-Link_Tapo_Android&appVer=2.2.39&netType=wifi&termID=9C-A5-C0-F2-4C-81&ospf=Android%206.0.1&brand=TPLINK&locale=th_TH";
+    this.headers = {
+      "Content-Type": "application/json; charset=UTF-8",
+      // "Content-Length": "250",
+      Host: "n-wap-gw.tplinkcloud.com",
+      Connection: "close",
+      "Accept-Encoding": "gzip, deflate",
+      "User-Agent": "okhttp/3.12.12",
+    };
+  }
+}
+
+class TapoLogin extends Config {
+  constructor(email, password, refreshTokenNeeded = false) {
+    super();
+    this.email = email;
+    this.password = password;
+    this.data = {
+      method: "login",
+      params: {
+        appType: "TP-Link_Tapo_Android",
+        appVersion: "2.2.39",
+        cloudPassword: password,
+        cloudUserName: email,
+        platform: "Android 6.0.1",
+        refreshTokenNeeded,
+        terminalUUID: "9C-A5-C0-F2-4C-81",
+      },
+    };
+    this.config = {
+      method: "post",
+      url: this.url,
+      headers: this.headers,
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+      data: this.data,
+    };
+    this.result = axios(this.config).then((item) => item.data.result);
+  }
+
+  getToken() {
+    return this.result.then((item) => item.token);
+  }
+
+  getAccountId() {
+    return this.result.then((item) => item.accountId);
+  }
+
+  getRegTime() {
+    return this.result.then((item) => item.regTime);
+  }
+
+  getCountryCode() {
+    return this.result.then((item) => item.countryCode);
+  }
+
+  getNickname() {
+    return this.result.then((item) => item.nickname);
+  }
+
+  getEmail() {
+    return this.result.then((item) => item.email);
+  }
+}
+
+class Tapo extends Config {
+  constructor(token) {
+    super();
+    this.token = token;
+  }
+
+  getCameraLists(index = 0, limit = 20) {
+    var data = {
+      method: "getDeviceListByPage",
+      params: {
+        deviceTypeList: ["SMART.TAPOPLUG", "SMART.TAPOBULB", "SMART.IPCAMERA"],
+        index,
+        limit,
+      },
+    };
+
+    var config = {
+      method: "post",
+      url: `${this.url}&token=${this.token}`,
+      headers: this.headers,
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+      data: data,
+    };
+    return axios(config).then((item) => item.data.result);
+  }
+
+  getCameraDetail(deviceId) {
+    var data = {
+      method: "passthrough",
+      params: {
+        deviceId,
+        requestData: {
+          method: "multipleRequest",
+          params: {
+            requests: [
+              {
+                method: "getConnectionType",
+                params: { network: { get_connection_type: "null" } },
+              },
+            ],
+          },
+        },
+      },
+    };
+
+    var config = {
+      method: "post",
+      url: `${this.url}&token=${this.token}`,
+      headers: this.headers,
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+      data: data,
+    };
+    return axios(config).then(
+      (item) => item.data.result.responseData.result.responses[0]
+    );
+  }
+
+  getHanddiskInfo(deviceId) {
+    var data = {
+      method: "passthrough",
+      params: {
+        deviceId,
+        requestData: {
+          method: "multipleRequest",
+          params: {
+            requests: [
+              {
+                method: "getLedStatus",
+                params: {
+                  harddisk_manage: {
+                    name: ["harddisk"],
+                    table: ["hd_info"],
+                  },
+                },
+              },
+            ],
+          },
+        },
+      },
+    };
+
+    var config = {
+      method: "post",
+      url: `${this.url}&token=${this.token}`,
+      headers: this.headers,
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+      data: data,
+    };
+    return axios(config).then(
+      (item) =>
+        item.data.result.responseData.result.responses[0].result.harddisk_manage
+          .hd_info[0].hd_info_1
+    );
+  }
+
+  getPlayback(deviceId,start_date="20210801",end_date="20210813") {
+    var data = {
+      method: "passthrough",
+      params: {
+        deviceId,
+        requestData: {
+          method: "multipleRequest",
+          params: {
+            requests: [
+              {
+                method: "searchDateWithVideo",
+                params: {
+                  playback: {
+                    search_year_utility: {
+                      channel: [0],
+                      end_date,
+                      start_date,
+                    },
+                  },
+                },
+              },
+            ],
+          },
+        },
+      },
+    };
+
+    var config = {
+      method: "post",
+      url: `${this.url}&token=${this.token}`,
+      headers: this.headers,
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+      data: data,
+    };
+    return axios(config).then((item) =>({
+      resultsCount:item.data.result.responseData.result.responses[0].result.playback.search_results.length,
+      results:item.data.result.responseData.result.responses[0].result.playback.search_results.map(
+        (item2, index) => item2[`search_results_${index + 1}`].date
+      )
+    }));
+  }
+}
+
+module.exports = { TapoLogin, Tapo };
